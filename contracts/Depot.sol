e4730319f57c3d5da4a81f0149f4e91f68b7fecc
deon <deonsummers91@gmail.com>
2017-07-22 15:43:45 -0500

    add testcases

--- contracts/Depot.sol
+++ contracts/Depot.sol
@@ -13,7 +13,7 @@ contract Depot is StandardToken {
 
     struct Warehouse {
         uint spaceAvailable;
-        uint pricePerCubicFoot;
+        uint pricePerCubicFootPerHour;
         Types typeOfWarehouse;
         address owner;
     }
@@ -24,33 +24,36 @@ contract Depot is StandardToken {
     function Depot() {
     }
 
-    function addWarehouse(uint256 _cubicFeet, uint pricePerCubicFoot, bool isRefrigerated) not_warehouse {
+    function addWarehouse(uint256 _cubicFeet, uint pricePerCubicFootPerHour, bool isRefrigerated) not_warehouse {
         Types typeOfFridge = (isRefrigerated ? Types.Refridgerated : Types.Regular);
         totalSupply += _cubicFeet;
-        listOfWarehouses.push(Warehouse(_cubicFeet, pricePerCubicFoot, typeOfFridge, msg.sender));
+        listOfWarehouses.push(Warehouse(_cubicFeet, pricePerCubicFootPerHour, typeOfFridge, msg.sender));
     }
 
-
-    function purchaseWarehouseSpace(address warehouseAddress, uint amountOfSpace) payable {
+    function purchaseWarehouseSpace(address warehouseAddress, uint cubicFeet, uint amountOfHours) payable {
         Warehouse storage warehouse = getWarehouseByAddress(warehouseAddress);
-        warehouse.spaceAvailable = 600000;
+        //Would probably import SafeMath module to multiply but MEH
+        // if(msg.value != (cubicFeet * amountOfHours * warehouse.pricePerCubicFootPerHour)) throw;  
+        // if(warehouse.spaceAvailable < cubicFeet) throw;
+        // warehouse.owner.transfer(msg.value);
+        // warehouse.spaceAvailable -= cubicFeet;
     }
 
     /** GETTER METHODS **/
     function warehouses() constant returns (uint[], uint[], uint[], address[]) {
         uint[] memory _spaceAvailable = new uint[](listOfWarehouses.length);
-        uint[] memory _pricePerCubicFoot = new uint[](listOfWarehouses.length);
+        uint[] memory _pricePerCubicFootPerHour = new uint[](listOfWarehouses.length);
         uint[] memory _typeOfWarehouse = new uint[](listOfWarehouses.length);
         address[] memory _owner = new address[](listOfWarehouses.length);
 
         for (var i = 0; i < listOfWarehouses.length; i++) {
             _spaceAvailable[i] = listOfWarehouses[i].spaceAvailable;
             _typeOfWarehouse[i] = uint(listOfWarehouses[i].typeOfWarehouse);
-            _pricePerCubicFoot[i] = listOfWarehouses[i].pricePerCubicFoot;
+            _pricePerCubicFootPerHour[i] = listOfWarehouses[i].pricePerCubicFootPerHour;
             _owner[i] = listOfWarehouses[i].owner;
         }
 
-        return (_spaceAvailable, _typeOfWarehouse, _pricePerCubicFoot, _owner);
+        return (_spaceAvailable, _typeOfWarehouse, _pricePerCubicFootPerHour, _owner);
     }
 
     /** INTERNAL METHODS **/
@@ -61,6 +64,7 @@ contract Depot is StandardToken {
         throw;
     }
 
+    /** Do not accept ether **/
     function () payable {
         throw;
     }
--- migrations/3_add_initial_data.js
+++ migrations/3_add_initial_data.js
@@ -3,6 +3,6 @@ var Depot = artifacts.require("./Depot.sol");
 module.exports = function(deployer) {
   Depot.deployed()
     .then((instance) => {
-      instance.addWarehouse(1000, 10, true);
+      instance.addWarehouse(100, 1, true);
     });
 };
