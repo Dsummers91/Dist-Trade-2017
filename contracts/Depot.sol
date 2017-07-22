d712749800316164e24b7a32220b903e163173a7
deon <deonsummers91@gmail.com>
2017-07-22 14:23:33 -0500

    initial token implementation

--- contracts/Depot.sol
+++ contracts/Depot.sol
@@ -3,23 +3,33 @@ pragma solidity 0.4.11;
 import 'tokens/StandardToken.sol';
 
 contract Depot {    
+    enum Types {Regular, Refridgerated}
     string public name = 'Depot';                   
     uint8 public decimals = 0;               
     string public symbol= 'Dpt';           
     string public version = 'H0.1';
-    enum Types {Regular, Refridgerated}
+    
+    Warehouse[] listOfWarehouses;
 
     struct Warehouse {
-        uint spaceAvailable,
-        Types typeOfWarehouse,
+        uint spaceAvailable;
+        Types typeOfWarehouse;
+        address owner;
     }
 
 
     function Depot() {
     }
 
-    function warehouseAddSpace(uint cubicFeet, uint type) {
-        balances[msg.sender] += cubicFeet;
+    function addWarehouse(uint256 _cubicFeet, bool isRefrigerated) not_warehouse {
+        Types typeOfFridge = (isRefrigerated ? Types.Refridgerated : Types.Regular);
+        listOfWarehouses.push(Warehouse(_cubicFeet, typeOfFridge, msg.sender));
+    }
 
+    modifier not_warehouse {
+        for (var i = 0; i < listOfWarehouses.length; i++) {
+            if(listOfWarehouses[i].owner == msg.sender) throw;
+        }
+        _;
     }
 }
